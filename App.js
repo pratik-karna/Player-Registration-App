d0c8b6b8784fb80abb74d61bcf52a2b490352cc2
Pratik Karna <99958114+pratik-karna@users.noreply.github.com>
2023-01-21 10:18:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigator from './src/Navigations';
+
+const App = () => {
+
+  return (
+    <NavigationContainer>
+      <TabNavigator/>
+    </NavigationContainer>
+  );
+};
+export default App;
